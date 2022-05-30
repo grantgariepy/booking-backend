156897d85c11d60a208eb50120175a3886b1ee03
grantgariepy <grantgariepy@protonmail.com>
2022-05-30 10:55:08 -0600

    register and login

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,13 +1,11 @@
 import express from "express";
+import { login, register } from "../controlers/auth.js";
 
 const router = express.Router();
 
-router.get("/", (req,res)=>{
-    res.send("Hello this is auth endpoint!")
-})
-router.get("/register", (req,res)=>{
-    res.send("Hello this is auth/register endpoint!")
-})
 
-export default router
+router.post("/register", register);
+router.post("/login", login);
+
+export default router;
 
