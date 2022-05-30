1b3ea72faa08d5fca38984b87c40321ed075ada8
grantgariepy <grantgariepy@protonmail.com>
2022-05-30 09:43:14 -0600

    models

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoute  from "./routes/auth.js"
+import usersRoute  from "./routes/users.js"
+import hotelsRoute  from "./routes/hotels.js"
+import roomsRoute  from "./routes/rooms.js"
 
 const app = express();
 dotenv.config()
@@ -18,13 +22,13 @@ mongoose.connection.on("disconnected", ()=>{
     console.log("mondoDB disconnected!")
 })
 
-mongoose.connection.on("connected", ()=>{
-    console.log("mondoDB connected!")
-})
+//middlewares
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
 
-app.get("/users", (req,res)=>{
-    res.send("hello first request!")
-})
+
 
 app.listen(8800, ()=>{
     connect()
